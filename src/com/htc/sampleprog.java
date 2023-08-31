d18da0b42642d7ad16ed20daf36db81c92eb3075
manivannan19 <mani11mer023@gmail.com>
2023-08-31 13:03:42 +0530

    code

---
+++ src/com/htc/sampleprog.java
@@ -0,0 +1,16 @@
+package com.htc;
+
+public class sampleprog {
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 0; i <= 100; i++) {
+			if (i % 2 != 0) {
+				count++;
+
+			}
+
+		}
+		System.out.println(count);
+	}
+
+}
